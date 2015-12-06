4522f1610a807e505092c915b9e1f6e9d87d3f74
annalyzin <annalyzin@users.noreply.github.com>
2015-12-06 15:40:55 +0800

    Update script.R

--- script.R
+++ script.R
@@ -9,7 +9,7 @@ on.exit(par(default.par))
 ########################################
 # Setup
 ########################################
-set.seed(55)
+set.seed(28)
 
 # number of clusters
 nc = 2
@@ -35,7 +35,7 @@ yp[yp < 0.05 | yp > 0.95] = NA
 ########################################
 # Iteration
 ########################################
-set.seed(100)
+set.seed(234)
 
 # set random pseudo-center
 xpc <- sample(seq(0.2, 0.8, by=0.1), 2)
@@ -51,42 +51,42 @@ points(xpc,ypc, pch=16, asp=1, cex=2) # centers
 par(ask=TRUE) # pause each time plot is generated
 # start iteration
 for (i in 1:5)  {
-
+  
   # assign points to clusters
   z <- deldir(xpc, ypc, rw=c(0,1,0,1))
   w <- tile.list(z)
-
-
+  
+  
   # plot pseudo-centers and points with boundaries colored
   plot(w, pch=NA, fillcol = c("#33ccff", "#ff9999"), close = TRUE)  # voronoi boundaries
   points(xpc,ypc, pch=16, cex=2) # centers
   points(xp,yp, pch=4, cex=2) # points
-
-
+  
+  
   # re-assign cluster members
   cluster <-  NA
   boundary <-  (z$dirsgs$y2 - z$dirsgs$y1) * xp + z$dirsgs$y1
   cluster[yp > boundary] = 2
   cluster[yp <= boundary] = 1
-
+  
   # re-locate cluster center
   xpcnew <- xpc
   ypcnew <- ypc
-
+  
   for (i in 1:2)  {
     xpcnew[i] <- mean(xp[cluster==i], na.rm = T)
     ypcnew[i] <- mean(yp[cluster==i], na.rm = T)
   }
-
+  
   # plot new cluster center with old boundaries
   plot(w, pch=NA, fillcol = c("#33ccff", "#ff9999"), close = TRUE)  # voronoi boundaries
   points(xpcnew,ypcnew, pch=16, cex=2) # centers
   points(xp,yp, pch=4, cex=2) # points
-
+  
   # stop the iteration if cluster centers have stabilized
   if (all(xpcnew != xpc) & all(xpcnew != xpc))  {
     xpc <- xpcnew
     ypc <- ypcnew
   } else break;
-
+  
 }
